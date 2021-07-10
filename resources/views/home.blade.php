226f4c7dfa0b9ce7a6774b64f7710d9508637b0c
Annie Lu <annykarta8@gmail.com>
2021-07-10 23:49:54 +0800

    增加登入與註冊功能(jetstream)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -85,6 +85,23 @@ https://templatemo.com/tm-558-klassy-cafe
                             </li>
                             <!-- <li class=""><a rel="sponsored" href="https://templatemo.com" target="_blank">External URL</a></li> -->
                             <li class="scroll-to-section"><a href="#reservation">Contact Us</a></li>
+                            <li>
+                                @if (Route::has('login'))
+                                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                                    @auth
+                                    <a href="{{ url('/dashboard') }}"
+                                        class="text-sm text-gray-700 underline">Dashboard</a>
+                                    @else
+                                    <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+
+                                    @if (Route::has('register'))
+                                    <a href="{{ route('register') }}"
+                                        class="ml-4 text-sm text-gray-700 underline">Register</a>
+                                    @endif
+                                    @endauth
+                                </div>
+                                @endif
+                            </li>
                         </ul>
                         <a class='menu-trigger'>
                             <span>Menu</span>
