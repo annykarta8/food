d209a56d53e009568c69e02d275afdbde1ae5b04
Annie Lu <annykarta8@gmail.com>
2021-07-11 10:21:42 +0800

    將dashboard換成Jetstream登入介面

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -89,27 +89,30 @@ https://templatemo.com/tm-558-klassy-cafe
                                 @if (Route::has('login'))
                                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                                     @auth
-                                    <a href="{{ url('/dashboard') }}"
-                                        class="text-sm text-gray-700 underline">Dashboard</a>
-                                    @else
-                                    <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+                            <li>
+                                <x-app-layout>
 
-                                    @if (Route::has('register'))
-                                    <a href="{{ route('register') }}"
-                                        class="ml-4 text-sm text-gray-700 underline">Register</a>
-                                    @endif
-                                    @endauth
-                                </div>
-                                @endif
+                                </x-app-layout>
                             </li>
-                        </ul>
-                        <a class='menu-trigger'>
-                            <span>Menu</span>
-                        </a>
-                        <!-- ***** Menu End ***** -->
-                    </nav>
+                            @else
+                            <li><a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a></li>
+
+                            @if (Route::has('register'))
+                            <li><a href="{{ route('register') }}"
+                                    class="ml-4 text-sm text-gray-700 underline">Register</a></li>
+                            @endif
+                            @endauth
                 </div>
+                @endif
+                </li>
+                </ul>
+                {{--  <a class='menu-trigger'>
+                    <span>Menu</span>
+                </a>  --}}
+                <!-- ***** Menu End ***** -->
+                </nav>
             </div>
+        </div>
         </div>
     </header>
     <!-- ***** Header Area End ***** -->
